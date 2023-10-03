84e435f0b4db5b5a2d85a59e86aac519665d5bc7
Yarnib <138246961+Yarnib@users.noreply.github.com>
2023-10-03 02:03:28 +0000

    Update

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,56 @@
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
+
+#app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///starwars.db'
 db = SQLAlchemy()
+#api = Api(app)
+
+# Define database models
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    gender = db.Column(String(250), nullable=False)
+    birth = db.Column(String(250), nullable=False)
+    height = db.Column(Integer)
+    skin_color = db.Column(String(250), nullable=False)
+    mass = db.Column(Integer)
+    hair_color = db.Column(String(250), nullable=False)
+    eye_color = db.Column(String(250), nullable=False)
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    population = db.Column(Integer)
+    terrain = db.Column(String(250), nullable=False)
+    climate = db.Column(String(250), nullable=False)
+    diameter = db.Column(Integer)
+    gravity = db.Column(String(250), nullable=False)
+
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255))
+    model = db.Column(String(250), nullable=False)
+    cost = db.Column(Integer)
+    speed = db.Column(Integer)
+    passengers = db.Column(Integer)
+    vehicle_class = db.Column(String(250), nullable=False)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(255))
+    firstname = db.Column(String(250), nullable=False)
+    lastname = db.Column(String(250), nullable=False)
+    email = db.Column(String(250), nullable=False)
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'))
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+
--- src/api/routes.py
+++ src/api/routes.py
@@ -1,18 +1,108 @@
-"""
+"""characters
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
-from api.utils import generate_sitemap, APIException
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from .models import db, People, Planet, Vehicle, User, Favorite
 
 api = Blueprint('api', __name__)
 
+# Define your new routes and endpoints here
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/characters', methods=['GET'])
+def get_characters():
+    people = People.query.all()
+    return jsonify([person.name for person in people])
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+@api.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    return jsonify([planet.name for planet in planets])
 
-    return jsonify(response_body), 200
+@api.route('/vehicles', methods=['GET'])
+def get_vehicles():
+    vehicles = Vehicle.query.all()
+    return jsonify([vehicle.name for vehicle in vehicles])
+
+@api.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.username for user in users])
+
+@api.route('/favorites/<string:category>/<int:item_id>', methods=['GET', 'POST', 'DELETE'])
+@jwt_required()  # Requires authentication (JWT)
+def manage_favorites(category, item_id):
+    # Get the current user's identity from the JWT token
+    current_user = get_jwt_identity()
+
+    # Check if the provided category is valid (e.g., 'planets', 'characters', 'vehicles')
+    valid_categories = ['planets', 'characters', 'vehicles']
+    if category not in valid_categories:
+        return jsonify({"error": "Invalid category"}), 400
+
+    if request.method == 'GET':
+        # Implement logic to retrieve favorites for the current user
+        user_favorites = Favorite.query.filter_by(user_id=current_user['id'], category=category).all()
+        
+        # Create a list of favorite items (e.g., planet names, character names)
+        favorites = []
+        for favorite in user_favorites:
+            item = None
+            if category == 'planets':
+                item = Planet.query.get(favorite.item_id)
+            elif category == 'characters':
+                item = People.query.get(favorite.item_id)
+            elif category == 'vehicles':
+                item = Vehicle.query.get(favorite.item_id)
+            
+            if item:
+                favorites.append({"category": category, "name": item.name})
+        
+        return jsonify(favorites)
+
+    elif request.method == 'POST':
+        # Check if the item with the given ID exists in the specified category
+        item = None
+        if category == 'planets':
+            item = Planet.query.get(item_id)
+        elif category == 'characters':
+            item = People.query.get(item_id)
+        elif category == 'vehicles':
+            item = Vehicle.query.get(item_id)
+        
+        if not item:
+            return jsonify({"error": "Item not found"}), 404
+        
+        # Add the item as a favorite for the current user
+        favorite = Favorite(user_id=current_user['id'], category=category, item_id=item_id)
+        db.session.add(favorite)
+        db.session.commit()
+        
+        return jsonify({"message": "Favorite added"})
+
+    elif request.method == 'DELETE':
+        # Check if the item with the given ID exists in the specified category
+        item = None
+        if category == 'planets':
+            item = Planet.query.get(item_id)
+        elif category == 'characters':
+            item = People.query.get(item_id)
+        elif category == 'vehicles':
+            item = Vehicle.query.get(item_id)
+        
+        if not item:
+            return jsonify({"error": "Item not found"}), 404
+        
+        # Check if the item is in the user's favorites
+        favorite = Favorite.query.filter_by(user_id=current_user['id'], category=category, item_id=item_id).first()
+        if not favorite:
+            return jsonify({"error": "Favorite not found"}), 404
+        
+        # Delete the favorite item for the current user
+        db.session.delete(favorite)
+        db.session.commit()
+        
+        return jsonify({"message": "Favorite deleted"})
+
+# Add more routes and endpoints as needed
+
